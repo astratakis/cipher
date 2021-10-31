9b2e3e2f0b3ddedaf231fed9a71e93199dd11d44
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-10-31 18:45:17 +0200

    Creating better scripts

--- scripts/random_capital.sh
+++ scripts/random_capital.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 NUMBER=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 3)
 if [ "$NUMBER" == "" ]; then
   NUMBER=0
--- scripts/testset_creator.sh
+++ scripts/testset_creator.sh
@@ -1,10 +1,70 @@
 #!/bin/bash
 
-touch testset_3.txt
+# Program arguments:
+#	$1 must be a positive integer
+#	$2 must be a number in [1, 3]
+#	$3 must be a positive integer
+#	$4 must be a string with a .txt file extension
 
-echo 10000 > testset_3.txt
+if [[ $# -ne 4 ]]
+then
+	echo "Usage: $0 <T> <Alg> <N> <F>"
+	echo "OPTIONS:"
+	echo "	<T>:		The number of test cases."
+	echo "	<Alg>:		The algorithm choice."
+	echo "		1:	OTP."
+	echo "		2:	Caesar's."
+	echo "		3:	Vigenere's."
+	echo "	<N>:		The maximum string length."
+	echo "	<F>: 		The output file name."
+	
+	echo
+	echo "Example: $0 100 3 100 testset_1.txt"
+	echo "This will create 100 test cases for Vigenere's algorithm with maximum string length 100 and the test file will be named testset_1.txt"
+	exit 1
+fi
 
-for i in {1..10000}
+number_format='^[0-9]+$'
+letter_format='[a-zA-Z]'
+
+if ! [[ $1 =~ $number_format ]] || [[ $1 -lt 1 ]]
+then
+	echo "[ERROR]: Expected positive integer <T>"
+	exit 1
+fi
+
+if ! [[ $2 =~ $number_format ]] || [[ $2 -lt 1 ]] || [[ $2 -gt 3 ]]
+then
+	echo "[ERROR]: Expected integer <Alg> in range: [1, 3] inclusive"
+	exit 1
+fi
+
+if ! [[ $3 =~ $number_format ]] || [[ $3 -lt 1 ]]
+then
+	echo "[ERROR]: Expected positive integer <N>"
+	exit 1
+fi
+
+if ! [[ $4 =~ $letter_format ]]
+then
+	echo "[ERROR]: Expected string <F>"
+fi
+
+# Starting the process of test creation
+
+touch $4
+
+echo $1 > $4
+
+for ((i=1; i<=$1; i++));
 do
-    /bin/bash random_capital.sh >> testset_3.txt
+	if [[ $2 -eq 1 ]]
+		# OTP random testset
+	elif [[ $2 -eq 2 ]]
+		# Caesar's random testset
+	else
+		# Vigenere's random testset
+
+	fi	
 done
+
