28e35f9e625db448c892f05a9d02a77a1052315a
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-10-31 19:51:38 +0200

    Created output for test sets 3, 4 and implemented the test functions

--- test_functions.c
+++ test_functions.c
@@ -194,9 +194,115 @@ int vigeneres_test_2(void) {
 }
 
 int vigeneres_test_3(void) {
+    FILE *in = fopen("vigeneres_tests/testset_3.txt", "r");
+    FILE *out = fopen("vigeneres_tests/testset_3_output.txt", "r");
 
+    int test_cases;
+    fscanf(in, "%d", &test_cases);
+
+    for (int tt=1; tt<=test_cases; tt++) {
+        int input_size;
+        fscanf(in, "%d", &input_size);
+
+        char *input = malloc(input_size + 1);
+        fscanf(in, "%s", input);
+
+        int key_size;
+        fscanf(in, "%d", &key_size);
+
+        char *key = malloc(key_size + 1);
+        fscanf(in, "%s", key);
+
+        char *encrypted = encrypt_vigeneres(input, key);
+
+        char *output = malloc(input_size + 1);
+        fscanf(out, "%s", output);
+
+        if (strcmp(encrypted, output) != 0) {
+            free(input);
+            free(output);
+            free(key);
+            free(encrypted);
+            fclose(in);
+            fclose(out);
+            return FAILURE;
+        }
+
+        char *decrypted = decrypt_vigeneres(encrypted, key);
+
+        if (strcmp(decrypted, input) != 0) {
+            free(input);
+            free(output);
+            free(key);
+            free(encrypted);
+            free(decrypted);
+            fclose(in);
+            fclose(out);
+            return FAILURE;
+        }
+
+        free(input);
+        free(output);
+        free(key);
+        free(encrypted);
+        free(decrypted);
+    }
+    return SUCCESS;
 }
 
 int vigeneres_test_4(void) {
+    FILE *in = fopen("vigeneres_tests/testset_4.txt", "r");
+    FILE *out = fopen("vigeneres_tests/testset_4_output.txt", "r");
 
+    int test_cases;
+    fscanf(in, "%d", &test_cases);
+
+    for (int tt=1; tt<=test_cases; tt++) {
+        int input_size;
+        fscanf(in, "%d", &input_size);
+
+        char *input = malloc(input_size + 1);
+        fscanf(in, "%s", input);
+
+        int key_size;
+        fscanf(in, "%d", &key_size);
+
+        char *key = malloc(key_size + 1);
+        fscanf(in, "%s", key);
+
+        char *encrypted = encrypt_vigeneres(input, key);
+
+        char *output = malloc(input_size + 1);
+        fscanf(out, "%s", output);
+
+        if (strcmp(encrypted, output) != 0) {
+            free(input);
+            free(output);
+            free(key);
+            free(encrypted);
+            fclose(in);
+            fclose(out);
+            return FAILURE;
+        }
+
+        char *decrypted = decrypt_vigeneres(encrypted, key);
+
+        if (strcmp(decrypted, input) != 0) {
+            free(input);
+            free(output);
+            free(key);
+            free(encrypted);
+            free(decrypted);
+            fclose(in);
+            fclose(out);
+            return FAILURE;
+        }
+
+        free(input);
+        free(output);
+        free(key);
+        free(encrypted);
+        free(decrypted);
+    }
+    return SUCCESS;
 }
