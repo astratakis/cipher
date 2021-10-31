d5c2d5622f9e137857d5b4c7bc9bc76d256ec55e
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-10-31 18:45:04 +0200

    Minor changes

--- test_functions.h
+++ test_functions.h
@@ -1,5 +1,9 @@
+#ifndef __TEST_FUNCTIONS__
+#define __TEST_FUNCTIONS__
 
 int check_tabula_recta(void);
 int vigeneres_sample_encrypt_test(void);
 int vigeneres_sample_decrypt_test(void);
 int vigeneres_sample_both_test(void);
+
+#endif
