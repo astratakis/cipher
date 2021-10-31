c683020a5573c2796e13247d05b60c5001487756
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-10-31 20:35:39 +0200

    push

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "simple_crypto.h"
 
 int main(int argc, char **argv) {
 
+    char input[1000];
+
+    printf("[OTP] input: ");
+    scanf("%s", input);
+    printf("[OTP] encrypted: \n");
+    printf("[OTP] decrypted: \n");
+
+    int shift;
+    memset(input, 0x0, 1000);
+
+    printf("[Caesars] input: ");
+    scanf("%s", input);
+    printf("[Caesars] key: ");
+    scanf("%d", &shift);
+    printf("[Caesars] encrypted: \n");
+    printf("[Caesars] decrypted: \n");
+
+    char key[1000];
+    memset(input, 0x0, 1000);
+
+    printf("[Vigenere] input: ");
+    scanf("%s", input);
+    printf("[Vigenere] key: ");
+    scanf("%s", key);
+
+    char *encrypted = encrypt_vigeneres(input, key);
+    printf("[Vigenere] encrypted: %s\n", encrypted);
+    char *decrypted = decrypt_vigeneres(encrypted, key);
+    printf("[Vigenere] decrypted: %s\n", decrypted);
     return 0;
 }
--- simple_crypto.h
+++ simple_crypto.h
@@ -13,3 +13,5 @@ ceasars_shift_vector create_ceasars_shift_vector();
 
 char *encrypt_vigeneres(const char *__plain_text, const char *__key);
 char *decrypt_vigeneres(const char *__encrypted, const char *__key);
+
+char *encrypt_otp();
